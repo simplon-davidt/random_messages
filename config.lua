7ca7c8cfa215b5cad37c92169937a27ef535289e
xisd <login@debian-compaq>
2017-01-21 14:06:52 +0100

    split file and config option for signs in chunk chance

--- config.lua
+++ config.lua
@@ -21,6 +21,13 @@ random_messages.options.signs = {
 		h_min = -200,
 		h_max = 200,
 
+		-- When a portion of map is generated, how many chances is there that signs will be placed in it
+		-- Number will be use a fracion of 1 ( 1/number ) OR a number beween 0 and 1
+		-- e.g : 10 will be used as 1/10 -- so its the same as writting 0.1
+		-- and it means 10% chances that signs will be placed in a a new chunk 
+		chance_of_signs_in_chunk = 1, 		-- 100%
+
+		-- If there is signs it this portions of map, how many will there be ?
 		-- Increase this value will to see more signs in the world
 		signs_per_chunk= 1,
 	}
